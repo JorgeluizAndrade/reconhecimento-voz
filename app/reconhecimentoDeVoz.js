5b2d442798c52e775045efc0290a970bfe6ac5d6
jojo <luizj1718@gmail.com>
2023-06-14 12:17:23 -0300

    primeiro commit

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,26 @@
+const elementoChute = document.getElementById('chute')
+
+
+window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeEvalido(chute)
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${chute}</span>
+    
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start())
---
+++ app/sortearNumero.js
@@ -0,0 +1,18 @@
+const menorValor = 1;
+const maiorValor = 100;
+
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * maiorValor + 1);
+}
+
+console.log("número secreto:", numeroSecreto);
+
+const elementoMenorValor = document.getElementById("menor-valor");
+
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById("maior-valor");
+
+elementoMaiorValor.innerHTML = maiorValor;
