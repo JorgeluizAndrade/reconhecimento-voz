d8283c60ba0166e00d876cc455de800eba043ed0
jojo <luizj1718@gmail.com>
2023-06-14 12:25:16 -0300

    retirando o console.log

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -7,8 +7,6 @@ function gerarNumeroAleatorio() {
   return parseInt(Math.random() * maiorValor + 1);
 }
 
-console.log("número secreto:", numeroSecreto);
-
 const elementoMenorValor = document.getElementById("menor-valor");
 
 elementoMenorValor.innerHTML = menorValor;
